01e683ffc129d93d97a98ca7aa80808c176f4d62
Martin Eisenbarth <eyesee@foobarlab.net>
2018-02-25 10:34:49 +0100

    naming changes

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/bash -uex
 
 # cleanup previous box, create new box and start ssh login
 vagrant suspend
 vagrant destroy -f
 vagrant box remove -f funtoo-stage3
-vagrant box add --name funtoo-stage3 foobarlab-funtoo-x86_64-generic_64-stage3.box
+vagrant box add --name funtoo-stage3 funtoo-x86_64-generic_64-stage3.box
 vagrant up
 vagrant ssh
