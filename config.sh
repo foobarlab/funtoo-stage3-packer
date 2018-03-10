82998f7347d7bd056972d5e5ff0de090b2247919
Martin Eisenbarth <eyesee@foobarlab.net>
2018-03-10 15:42:01 +0100

    switch to pure64 build

--- config.sh
+++ config.sh
@@ -8,9 +8,9 @@ export BUILD_STAGE3_FILE="stage3-latest.tar.xz"
 #export BUILD_FUNTOO_DOWNLOADPATH="https://build.funtoo.org/funtoo-current/x86-64bit/generic_64"     # use this for 32bit/64bit multilib
 export BUILD_FUNTOO_DOWNLOADPATH="https://build.funtoo.org/funtoo-current/pure64/generic_64-pure64"  # use this for 64bit only
 
-export BUILD_BOX_NAME="funtoo-stage3"
-export BUILD_BOX_DESCRIPTION="Funtoo stage3 installation"
-export BUILD_OUTPUT_FILE="funtoo-x86_64-generic_64-stage3.box"
+export BUILD_BOX_NAME="funtoo-stage3-pure64"
+export BUILD_BOX_DESCRIPTION="Funtoo stage3 installation (pure64, generic64)"
+export BUILD_OUTPUT_FILE="funtoo-pure64-generic_64-stage3.box"
 export BUILD_GUEST_TYPE="Gentoo_64"
 export BUILD_GUEST_CPUS="4"
 export BUILD_GUEST_MEMORY="2048"
