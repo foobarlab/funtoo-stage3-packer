6aa88b5e0e2d6cb587030a99733b9b9f90517bb4
Martin Eisenbarth <eyesee@foobarlab.net>
2018-03-10 15:42:18 +0100

    cleanup

--- scripts/03-stage3.sh
+++ scripts/03-stage3.sh
@@ -7,14 +7,6 @@ then
 else
 	echo "File '$BUILD_STAGE3_PATH' does not exist. Aborting"
 	exit 1
-
-#	wget $BUILD_STAGE3_URL
-#	if [ $? -ne 0 ]; then
-#    	echo "Could not download '$BUILD_STAGE3_URL'. Exit code from wget was $?."
-#    	exit 1
-#    fi
-#	mv $BUILD_STAGE3_FILE /mnt/funtoo
-
 fi
 
 cd /mnt/funtoo
