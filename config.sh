a71eedcd539574771b7e046a8d50e74ab97017e7
Martin Eisenbarth <eyesee@foobarlab.net>
2018-03-10 15:55:30 +0100

    added commented settings for multilib build (x86-64bit)

--- config.sh
+++ config.sh
@@ -5,15 +5,21 @@ export BUILD_SYSTEMRESCUECD_REMOTE_HASH="d76d9444a73ce2127e489f54b0ce1cb9057ae47
 
 export BUILD_STAGE3_FILE="stage3-latest.tar.xz"
 
-#export BUILD_FUNTOO_DOWNLOADPATH="https://build.funtoo.org/funtoo-current/x86-64bit/generic_64"     # use this for 32bit/64bit multilib
-export BUILD_FUNTOO_DOWNLOADPATH="https://build.funtoo.org/funtoo-current/pure64/generic_64-pure64"  # use this for 64bit only
-
-export BUILD_BOX_NAME="funtoo-stage3-pure64"
-export BUILD_BOX_DESCRIPTION="Funtoo stage3 installation (pure64, generic64)"
-export BUILD_OUTPUT_FILE="funtoo-pure64-generic_64-stage3.box"
 export BUILD_GUEST_TYPE="Gentoo_64"
 export BUILD_GUEST_CPUS="4"
 export BUILD_GUEST_MEMORY="2048"
+
+# x86-64bit/generic64 build (multilib)
+#export BUILD_FUNTOO_DOWNLOADPATH="https://build.funtoo.org/funtoo-current/x86-64bit/generic_64"
+#export BUILD_BOX_NAME="funtoo-stage3"
+#export BUILD_BOX_DESCRIPTION="Funtoo stage3 installation (x86-64bit, generic64)"
+#export BUILD_OUTPUT_FILE="funtoo-x86_64-generic_64-stage3.box"
+
+# pure64/generic64 build
+export BUILD_FUNTOO_DOWNLOADPATH="https://build.funtoo.org/funtoo-current/pure64/generic_64-pure64"
+export BUILD_BOX_NAME="funtoo-stage3-pure64"
+export BUILD_BOX_DESCRIPTION="Funtoo stage3 installation (pure64, generic64)"
+export BUILD_OUTPUT_FILE="funtoo-pure64-generic_64-stage3.box"
 
 echo "Executing $0 ..."
 echo "=== Build settings ============================================================="
