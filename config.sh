c3e38e58fe7b7d8ae55ffaf2fb4f8e2b833d3727
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-03-22 11:11:01 +0100

    determine release infos from stage3 tarball

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export BUILD_BOX_NAME="funtoo-stage3"
-export BUILD_BOX_VERSION="0"			# FIXME determine actual version dynamically? (see: '/etc/os-release' and '/etc/gentoo-release' inside stage3 tarball)
+export BUILD_BOX_VERSION="0"
 export BUILD_GUEST_TYPE="Gentoo_64"
 export BUILD_GUEST_CPUS="4"
 export BUILD_GUEST_MEMORY="2048"
@@ -23,7 +23,20 @@ export BUILD_OUTPUT_FILE="$BUILD_BOX_NAME.box"
 export BUILD_SYSTEMRESCUECD_FILE="systemrescuecd-x86-5.2.1.iso"
 export BUILD_SYSTEMRESCUECD_REMOTE_HASH="d76d9444a73ce2127e489f54b0ce1cb9057ae470459dc3fb32e8c916f7cbfe2e"
 
-echo "Executing $0 ..."
-echo "=== Build settings ============================================================="
-env | grep BUILD_
-echo "================================================================================"
+# dynamically set actual BUILD_BOX_VERSION and some additional vars from extracted release info 
+if [ -f ./release ]; then
+	while read line; do
+		line_name=`echo $line |cut -d "=" -f1`
+		line_value=`echo $line |cut -d "=" -f2`
+		export "BUILD_RELEASE_$line_name=$line_value"
+	done < ./release
+	export BUILD_BOX_VERSION=`echo $BUILD_RELEASE_VERSION | sed -e 's/\-/./g'`
+fi
+
+# output info only if there is no argument given
+if [ $# -eq 0 ]; then
+	echo "Executing $0 ..."
+	echo "=== Build settings ============================================================="
+	env | grep BUILD_
+	echo "================================================================================"
+fi
