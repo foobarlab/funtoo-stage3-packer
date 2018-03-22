f241bb7c913916216bdd0ad36b9dc25381258144
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-03-22 11:32:03 +0100

    more generic description

--- config.sh
+++ config.sh
@@ -7,30 +7,30 @@ export BUILD_GUEST_CPUS="4"
 export BUILD_GUEST_MEMORY="2048"
 
 export BUILD_STAGE3_FILE="stage3-latest.tar.xz"
+export BUILD_BOX_DESCRIPTION="$BUILD_BOX_NAME build @$(date --iso-8601=seconds)"
 
 # x86-64bit/generic64 build (multilib)
 #export BUILD_FUNTOO_DOWNLOADPATH="https://build.funtoo.org/funtoo-current/x86-64bit/generic_64"
 #export BUILD_FUNTOO_DOWNLOADPATH="https://ftp.osuosl.org/pub/funtoo/funtoo-current/x86-64bit/generic_64"
-#export BUILD_BOX_DESCRIPTION="Funtoo stage3 installation (x86-64bit, generic64)"
 
 # pure64/generic64 build
 #export BUILD_FUNTOO_DOWNLOADPATH="https://build.funtoo.org/funtoo-current/pure64/generic_64-pure64"
 export BUILD_FUNTOO_DOWNLOADPATH="https://ftp.osuosl.org/pub/funtoo/funtoo-current/pure64/generic_64-pure64"
-export BUILD_BOX_DESCRIPTION="Funtoo stage3 installation (pure64, generic64)"
 
 export BUILD_OUTPUT_FILE="$BUILD_BOX_NAME.box"
 
 export BUILD_SYSTEMRESCUECD_FILE="systemrescuecd-x86-5.2.1.iso"
 export BUILD_SYSTEMRESCUECD_REMOTE_HASH="d76d9444a73ce2127e489f54b0ce1cb9057ae470459dc3fb32e8c916f7cbfe2e"
 
-# dynamically set actual BUILD_BOX_VERSION and some additional vars from extracted release info 
+# dynamically set actual version and description from extracted release info 
 if [ -f ./release ]; then
 	while read line; do
 		line_name=`echo $line |cut -d "=" -f1`
-		line_value=`echo $line |cut -d "=" -f2`
+		line_value=`echo $line |cut -d "=" -f2 | sed -e 's/"//g'`
 		export "BUILD_RELEASE_$line_name=$line_value"
 	done < ./release
 	export BUILD_BOX_VERSION=`echo $BUILD_RELEASE_VERSION | sed -e 's/\-/./g'`
+	export BUILD_BOX_DESCRIPTION="$BUILD_BOX_NAME version $BUILD_BOX_VERSION ($BUILD_RELEASE_VERSION_ID) build @$(date --iso-8601=seconds)"
 fi
 
 # output info only if there is no argument given
