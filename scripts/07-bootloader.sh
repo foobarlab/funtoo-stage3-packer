e5e779827cf266ded60eeff727324cfa3c3abb79
Martin Eisenbarth <eyesee@foobarlab.net>
2018-10-05 01:01:51 +0200

    add depclean to fix boot-update being pulled in (just in case)

--- scripts/07-bootloader.sh
+++ scripts/07-bootloader.sh
@@ -5,6 +5,7 @@ if [ -z ${BUILD_RUN:-} ]; then
   exit 1
 fi
 
+# this is a workaround for now
 # upgrade to latest ego and re-sync
 chroot /mnt/funtoo /bin/bash -uex <<'EOF'
 emerge -s app-admin/ego
@@ -14,6 +15,7 @@ source /etc/profile
 etc-update --preen
 etc-update --automode -5
 ego sync
+emerge --depclean
 EOF
 
 # install bootloader
