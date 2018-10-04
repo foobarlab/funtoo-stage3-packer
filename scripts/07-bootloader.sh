1e917bdccbee68bc415e7eaccbb8413b00d3ce5c
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-10-04 15:54:54 +0200

    explicit install of grub (since this is not pulled in automatically)

--- scripts/07-bootloader.sh
+++ scripts/07-bootloader.sh
@@ -6,6 +6,7 @@ if [ -z ${BUILD_RUN:-} ]; then
 fi
 
 chroot /mnt/funtoo /bin/bash -uex <<'EOF'
+emerge -vt sys-boot/grub
 grub-install --target=i386-pc --no-floppy /dev/sda
 cat > /etc/boot.conf <<'DATA'
 boot {
