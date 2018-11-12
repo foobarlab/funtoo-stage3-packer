f4f5e77f5eafa0e6adde33e2ebb17013524ec784
Martin Eisenbarth <eyesee@foobarlab.net>
2018-11-12 12:15:28 +0100

    update wording, add note for upcoming lts kernel downgrade

--- clean_env.sh
+++ clean_env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ue
 
 command -v vagrant >/dev/null 2>&1 || { echo "Command 'vagrant' required but it's not installed.  Aborting." >&2; exit 1; }
+# FIXME command may be written as 'VBoxManage' instead of 'vboxmanage'
 command -v vboxmanage >/dev/null 2>&1 || { echo "Command 'vboxmanage' required but it's not installed.  Aborting." >&2; exit 1; }
 
 . config.sh
@@ -9,22 +10,23 @@ command -v vboxmanage >/dev/null 2>&1 || { echo "Command 'vboxmanage' required b
 . clean.sh
 
 # do some more system cleanup:
-# => suspend all vms as seen by the current user
+# => suspend all VMs as seen by the current user
 # => delete temporary files as seen by the current user
-echo "Suspend any running vagrant vms ..."
+echo "Suspend any running Vagrant VMs ..."
 vagrant global-status | awk '/running/{print $1}' | xargs -r -d '\n' -n 1 -- vagrant suspend
-echo "Prune invalid vagrant entries ..."
+echo "Prune invalid Vagrant entries ..."
 vagrant global-status --prune
-echo "Forcibly shutdown any running virtualbox vms ..."
+echo "Forcibly shutdown any running VirtualBox VMs ..."
 vboxmanage list runningvms | sed -r 's/.*\{(.*)\}/\1/' | xargs -L1 -I {} VBoxManage controlvm {} acpipowerbutton && true
 vboxmanage list runningvms | sed -r 's/.*\{(.*)\}/\1/' | xargs -L1 -I {} VBoxManage controlvm {} poweroff && true
-echo "Delete all inaccessible vms ..."
+echo "Delete all inaccessible VMs ..."
 vboxmanage list vms | grep "<inaccessible>" | sed -r 's/.*\{(.*)\}/\1/' | xargs -L1 -I {} vboxmanage unregistervm --delete {}
-echo "Force remove of appliance from Virtualbox folder ..."
+echo "Force remove of appliance from VirtualBox folder ..."
+# FIXME assumed path ~/.VirtualBox/Machines/ might be incorrect, better get this from VirtualBox config somehow
 rm -rf ~/.VirtualBox/Machines/$BUILD_BOX_NAME/
-echo "Delete temporary vagrant files ..."
+echo "Delete temporary Vagrant files ..."
 rm -rf ~/.vagrant.d/tmp/*
-echo "Current Status for Virtualbox (if any): "
+echo "Current Status for VirtualBox (if any): "
 vboxmanage list vms
 echo "Current Status for Vagrant (if any):"
 vagrant global-status
