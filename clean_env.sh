3557fde694e2937d6c4cdab391ed781512d97c08
Martin Eisenbarth <eyesee@foobarlab.net>
2020-06-19 15:41:20 +0200

    replace 'vboxmanage' executable with variable ($VBOXMANAGE) to allow changing name of the executable (this could be 'VBoxManage' instead of 'vboxmanage')

--- clean_env.sh
+++ clean_env.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -ue
 
+VBOXMANAGE=VBoxManage
+
 command -v vagrant >/dev/null 2>&1 || { echo "Command 'vagrant' required but it's not installed.  Aborting." >&2; exit 1; }
 # FIXME command may be written as 'VBoxManage' instead of 'vboxmanage'
-command -v vboxmanage >/dev/null 2>&1 || { echo "Command 'vboxmanage' required but it's not installed.  Aborting." >&2; exit 1; }
+command -v $VBOXMANAGE >/dev/null 2>&1 || { echo "Command '$VBOXMANAGE' required but it's not installed.  Aborting." >&2; exit 1; }
 
 . config.sh
 
@@ -17,17 +19,17 @@ vagrant global-status | awk '/running/{print $1}' | xargs -r -d '\n' -n 1 -- vag
 echo "Prune invalid Vagrant entries ..."
 vagrant global-status --prune
 echo "Forcibly shutdown any running VirtualBox VMs ..."
-vboxmanage list runningvms | sed -r 's/.*\{(.*)\}/\1/' | xargs -L1 -I {} VBoxManage controlvm {} acpipowerbutton && true
-vboxmanage list runningvms | sed -r 's/.*\{(.*)\}/\1/' | xargs -L1 -I {} VBoxManage controlvm {} poweroff && true
+$VBOXMANAGE list runningvms | sed -r 's/.*\{(.*)\}/\1/' | xargs -L1 -I {} VBoxManage controlvm {} acpipowerbutton && true
+$VBOXMANAGE list runningvms | sed -r 's/.*\{(.*)\}/\1/' | xargs -L1 -I {} VBoxManage controlvm {} poweroff && true
 echo "Delete all inaccessible VMs ..."
-vboxmanage list vms | grep "<inaccessible>" | sed -r 's/.*\{(.*)\}/\1/' | xargs -L1 -I {} vboxmanage unregistervm --delete {}
+$VBOXMANAGE list vms | grep "<inaccessible>" | sed -r 's/.*\{(.*)\}/\1/' | xargs -L1 -I {} $VBOXMANAGE unregistervm --delete {}
 echo "Force remove of appliance from VirtualBox folder ..."
 # FIXME assumed path ~/.VirtualBox/Machines/ might be incorrect, better get this from VirtualBox config somehow
 rm -rf ~/.VirtualBox/Machines/$BUILD_BOX_NAME/
 echo "Delete temporary Vagrant files ..."
 rm -rf ~/.vagrant.d/tmp/*
 echo "Current Status for VirtualBox (if any): "
-vboxmanage list vms
+$VBOXMANAGE list vms
 echo "Current Status for Vagrant (if any):"
 vagrant global-status
 echo "All done. Environment was cleaned. You may now run './build.sh' to build a fresh box."
