a6cf276f43a20c2d6d57df936785456036d9cae5
Martin Eisenbarth <eyesee@foobarlab.net>
2020-06-20 06:29:32 +0200

    change description

--- config.sh
+++ config.sh
@@ -40,8 +40,8 @@ if [[ -f ./release && -s release ]]; then
 	
 	BUILD_BOX_DESCRIPTION="Funtoo 1.4<br><br>$BUILD_BOX_NAME version $BUILD_BOX_VERSION ($BUILD_RELEASE_VERSION_ID)"
 	if [ -z ${BUILD_NUMBER+x} ] || [ -z ${BUILD_TAG+x} ]; then
-		# without build number 
-		BUILD_BOX_DESCRIPTION="$BUILD_BOX_DESCRIPTION custom build"
+		# without build number/tag
+		BUILD_BOX_DESCRIPTION="$BUILD_BOX_DESCRIPTION"
 	else
 		# for jenkins builds we got some additional information: BUILD_NUMBER, BUILD_ID, BUILD_DISPLAY_NAME, BUILD_TAG, BUILD_URL
 		BUILD_BOX_DESCRIPTION="$BUILD_BOX_DESCRIPTION build $BUILD_NUMBER ($BUILD_TAG)"
