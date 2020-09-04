076078abbd732ef9a7023e443b372f427c827bdf
Martin Eisenbarth <eyesee@foobarlab.net>
2020-09-04 06:30:06 +0200

    add make.conf intro

--- scripts/05-portage.sh
+++ scripts/05-portage.sh
@@ -6,6 +6,12 @@ if [ -z ${BUILD_RUN:-} ]; then
 fi
 
 cat <<'DATA' | tee -a /mnt/funtoo/etc/portage/make.conf
-ACCEPT_LICENSE="-* @FREE @BINARY-REDISTRIBUTABLE"
+# Contains local system settings for Portage system
+# Please consult /usr/share/portage/config/make.conf.example for a more
+# detailed example.
+
 USE="bindist"
+
+ACCEPT_LICENSE="-* @FREE @BINARY-REDISTRIBUTABLE"
+
 DATA
