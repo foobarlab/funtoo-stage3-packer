5ea157b69eebc9121eb2e5887d232541159a3298
Martin Eisenbarth <eyesee@foobarlab.net>
2020-10-06 10:49:36 +0200

    cleanup

--- scripts/06-update_world.sh
+++ scripts/06-update_world.sh
@@ -5,6 +5,7 @@ if [ -z ${BUILD_RUN:-} ]; then
   exit 1
 fi
 
+# update world, but skip kernel
 chroot /mnt/funtoo /bin/bash -uex <<'EOF'
 # initial sync
 ego sync
@@ -19,17 +20,5 @@ etc-update --automode -5
 gcc-config -l || gcc-config 1
 # re-sync
 ego sync
-EOF
-
-# prepare kernel config
-if [ -f ${SCRIPTS}/scripts/kernel.config ]; then
-	cp ${SCRIPTS}/scripts/kernel.config /mnt/funtoo/usr/src
-	cp /mnt/funtoo/usr/src/linux/.config /mnt/funtoo/usr/src/kernel.config.stage3-dist
-else
-	cp /mnt/funtoo/usr/src/linux/.config /mnt/funtoo/usr/src/kernel.config.stage3-dist
-fi
-
-# update world, but skip kernel
-chroot /mnt/funtoo /bin/bash -uex <<'EOF'
 emerge -vt --update --newuse --deep --with-bdeps=y @world --exclude="sys-kernel/debian-sources-lts"
 EOF
