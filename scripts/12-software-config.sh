f53477ef13a2fda33a19886f613c3995e242c0f4
Martin Eisenbarth <eyesee@foobarlab.net>
2021-05-05 21:55:59 +0200

    naming

--- scripts/12-software-config.sh
+++ scripts/12-software-config.sh
@@ -21,7 +21,7 @@ EOF
 rm -f /mnt/funtoo/etc/motd
 cat <<'DATA' | tee -a /mnt/funtoo/etc/motd
 
-Funtoo GNU/Linux Vagrant Box (BUILD_BOX_USERNAME/BUILD_BOX_NAME) - release BUILD_BOX_VERSION build BUILD_TIMESTAMP
+Funtoo Linux Vagrant Box (BUILD_BOX_USERNAME/BUILD_BOX_NAME) - release BUILD_BOX_VERSION build BUILD_TIMESTAMP
 
 DATA
 sed -i 's/BUILD_BOX_NAME/'"${BUILD_BOX_NAME:-}"'/g' /mnt/funtoo/etc/motd
@@ -32,7 +32,7 @@ cat /mnt/funtoo/etc/motd
 
 rm -f /mnt/funtoo/etc/issue
 cat <<'DATA' | tee -a /mnt/funtoo/etc/issue
-This is a Funtoo GNU/Linux Vagrant Box (BUILD_BOX_USERNAME/BUILD_BOX_NAME-BUILD_BOX_VERSION)
+This is a Funtoo Linux Vagrant Box (BUILD_BOX_USERNAME/BUILD_BOX_NAME-BUILD_BOX_VERSION)
 
 DATA
 sed -i 's/BUILD_BOX_VERSION/'$BUILD_BOX_VERSION'/g' /mnt/funtoo/etc/issue
