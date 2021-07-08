1d8ec4d8d2edd987f7f60eeae1b609e3570855bf
Martin Eisenbarth <eyesee@foobarlab.net>
2021-07-08 22:56:52 +0200

    wip

--- build.sh
+++ build.sh
@@ -8,6 +8,10 @@ echo "Executing $0 ..."
 
 require_commands vagrant packer wget sha256sum pv
 
+echo "------------------------------------------------------------------------"
+echo "   Building $BUILD_BOX_NAME v$BUILD_BOX_VERSION"
+echo "------------------------------------------------------------------------"
+
 if [ -f "$BUILD_SYSTEMRESCUECD_FILE" ]; then
 	echo "'$BUILD_SYSTEMRESCUECD_FILE' found. Skipping download ..."
 else
@@ -135,14 +139,14 @@ if [ "$BUILD_SKIP_VERSION_CHECK" = false ]; then
 	echo "Latest cloud version: $LATEST_CLOUD_VERSION"
 	
 	if [[ $BUILD_BOX_VERSION = $LATEST_CLOUD_VERSION ]]; then
-		echo "Same version already exists. Aborting build."
+		echo "Attention: an equal version number already exists. Hint: run './clean.sh' and try again. This will increment your build number automatically."
 		exit 0
 	else 
-		echo "Looks like we got a new version available. Proceeding build ..."
+		echo "Looks like we have a new version to provide. Proceeding build ..."
 	fi
 
 else
-	echo "Skipping cloud version check ..."
+	echo "Skipping cloud version check."
 fi
 
 cp $BUILD_STAGE3_FILE ./scripts
