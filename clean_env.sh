b5d8be5d0c13c39b7efa048050becb74b1a2ceab
Martin Eisenbarth <eyesee@foobarlab.net>
2021-07-13 22:48:59 +0200

    delete machine, skip hdd removal

--- clean_env.sh
+++ clean_env.sh
@@ -28,30 +28,34 @@ if [[ -z "$vbox_machine_id" || "$vbox_machine_id" = "" ]]; then
   echo "No machine named '$BUILD_BOX_NAME' found."
 else
   echo "Found machine UUID for '$BUILD_BOX_NAME': { $vbox_machine_id }"
-  echo "TODO: delete machine and all media"
-  # DEBUG:
-  #$vboxmanage showvminfo $vbox_machine_id
-  #$vboxmanage showvminfo $vbox_machine_id --machinereadable
+  echo ">>> Deleting machine ..."
+  $vboxmanage unregistervm --delete $vbox_machine_id >/dev/null 2>&1 || true
 fi
-echo ">>> Searching for forgotten VirtualBox HDDS named '${BUILD_BOX_NAME}.vdi' ..."
-vbox_hdd_found=$( $vboxmanage list hdds | grep "${BUILD_BOX_NAME}.vdi" || echo )
-if [[ -z "$vbox_hdd_found" || "$vbox_hdd_found" = "" ]]; then
-  echo "No HDD named '${BUILD_BOX_NAME}.vdi' found."
-else
-  echo "HDD found."
-  echo "TODO: Searching for HDD UUID ..."
-  # DEBUG:
-  #$vboxmanage list hdds
-  #$vboxmanage list hdds | grep -o "^UUID" | wc -l
-  #$vboxmanage list hdds | grep -on "^UUID.*"
-  #$vboxmanage list hdds | grep -on "^State:.*"
-  #$vboxmanage list hdds | grep -on "^Location:.*"
-  echo "TODO: Removing HDD from Media Manager ..."
-  #$vboxmanage closemedium disk $vbox_hdd_id --delete
-fi
+# TODO check if this is needed:
+#echo ">>> Searching for VirtualBox HDDS named '${BUILD_BOX_NAME}.vdi' ..."
+#vbox_hdd_found=$( $vboxmanage list hdds | grep "${BUILD_BOX_NAME}.vdi" || echo )
+#if [[ -z "$vbox_hdd_found" || "$vbox_hdd_found" = "" ]]; then
+#  echo "No HDD named '${BUILD_BOX_NAME}.vdi' found."
+#else
+#  echo "HDD found."
+#  echo "TODO: Searching for HDD UUID ..."
+#  # DEBUG:
+#  #$vboxmanage list hdds
+#  #$vboxmanage list hdds | grep -o "^UUID" | wc -l
+#  #$vboxmanage list hdds | grep -on "^UUID.*"
+#  #$vboxmanage list hdds | grep -on "^State:.*"
+#  #$vboxmanage list hdds | grep -on "^Location:.*"
+#  echo "TODO: Removing HDD from Media Manager ..."
+#  #$vboxmanage closemedium disk $vbox_hdd_id --delete
+#fi
 echo ">>> Delete all inaccessible VMs named '$BUILD_BOX_NAME' ..."
-vbox_inaccessible_vms=$( $vboxmanage list vms | grep "<inaccessible>" | grep "$BUILD_BOX_NAME" | sed -r 's/.*\{(.*)\}/\1/' )
-$vboxmanage unregistervm --delete "$vbox_inaccessible_vms" >/dev/null 2>&1 || true
+vbox_inaccessible_id=$( $vboxmanage list vms | grep "<inaccessible>" | grep "$BUILD_BOX_NAME" | sed -r 's/.*\{(.*)\}/\1/' )
+if [[ -z "$vbox_inaccessible_id" || "$vbox_inaccessible_id" = "" ]]; then
+  echo "No inaccessible machine named '$BUILD_BOX_NAME' found."
+else
+  echo "Found inaccessible machine UUID for '$BUILD_BOX_NAME': { vbox_inaccessible_id }"
+  $vboxmanage unregistervm --delete "$vbox_inaccessible_id" >/dev/null 2>&1 || true
+fi
 echo ">>> Force remove of appliance from VirtualBox machine folder ..."
 vboxmachinefolder=$( $vboxmanage list systemproperties | grep "Default machine folder" | cut -d ':' -f2 | sed -e 's/^\s*//g' )
 rm -rf "$vboxmachinefolder/$BUILD_BOX_NAME/" || true
